a8a30c5a5343b23bc278b491fd9c95cf755cebfd
ILIAS <dxzxstr@gmail.com>
2024-09-13 09:56:01 +0600

    exepted1002

---
+++ beecrowd/src/beec1002.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class beec1002 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double r = in.nextDouble();
+        double result = 3.14159 * r * r;
+
+        System.out.printf("A=%.4f\n",result);
+    }
+}
