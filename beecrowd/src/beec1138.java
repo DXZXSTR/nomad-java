cd71e0e2764279636964dbea6afda21c4ccfc56e
ILIAS <dxzxstr@gmail.com>
2024-10-25 08:11:11 +0600

    1024 1255 done, 1138 rename

--- beecrowd/src/beec1138.java
+++ beecrowd/src/beec1138.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class beecrowd1138 {
+public class beec1138 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
