d77bd8307d5e1b78f85c808c23f4aa3ca1999820
ILIAS <dxzxstr@gmail.com>
2024-10-25 07:17:56 +0600

    1253, 1237 1120 done

---
+++ problems/src/problem21.java
@@ -0,0 +1,8 @@
+public class problem21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a degree in Celsius: ");
+        double celsius = in.nextDouble();
+        double result = (9.0 / 5.0)
+    }
+}
